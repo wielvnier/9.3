9ee04f5b5dab04fbd6e8af98c6ccf4d7a64d4c1e
wielvnier <filipwielanier@gmail.com>
2017-05-11 15:21:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+
+var animal = "Zielone słonie";
+
+var animalUpperCased = animal.toUpperCase();
+
+var text = text.replace('Papugi', var animalUpperCased);
+
+console.log(text)
